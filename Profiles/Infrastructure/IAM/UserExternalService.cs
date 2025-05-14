02796496280c05e7b4892cd6a6f818644d5256c2
Mathias Jave Diaz <u202212721@upc.edu.pe>
2025-05-14 10:20:03 -0500

    fix: changed route typo error

--- Profiles/Infrastructure/IAM/UserExternalService.cs
+++ Profiles/Infrastructure/IAM/UserExternalService.cs
@@ -6,7 +6,7 @@ public class UserExternalService(HttpClient client) : IUserExternalService
 {
     public async Task<bool> UserExistsById(int userId)
     {
-        var url = "http://iam-api:8012/api/v1/users/" + userId + "exists";
+        var url = "http://iam-api:8012/api/v1/users/" + userId + "/exists";
         var response = await client.GetAsync(url);
         if (!response.IsSuccessStatusCode)
         {
