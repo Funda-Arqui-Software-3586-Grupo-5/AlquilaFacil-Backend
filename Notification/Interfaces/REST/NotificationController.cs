335e20779ba8ed5dfabcfbe04733d77f6181a8c3
Arian Rodriguez <arianmrv12@gmail.com>
2025-05-14 16:33:35 -0500

    add: notification http responses

--- Notification/Interfaces/REST/NotificationController.cs
+++ Notification/Interfaces/REST/NotificationController.cs
@@ -17,26 +17,38 @@ public class NotificationController(INotificationCommandService notificationComm
     [HttpPost]
     public async Task<IActionResult> SaveNotification([FromBody] CreateNotificationResource createNotificationResource)
     {
-        var command = CreateNotificationCommandFromResourceAssembler.ToCommandFromResource(createNotificationResource);
-        var notification = await notificationCommandService.Handle(command);
-        var notificationResource = NotificationResourceFromEntityAssembler.ToResourceFromEntity(notification);
-        return StatusCode(201, notificationResource);
+        try {
+            var command = CreateNotificationCommandFromResourceAssembler.ToCommandFromResource(createNotificationResource);
+            var notification = await notificationCommandService.Handle(command);
+            var notificationResource = NotificationResourceFromEntityAssembler.ToResourceFromEntity(notification);
+            return StatusCode(201, notificationResource);
+        }catch(Exception ex) {
+            return BadRequest(new { message = ex.Message });
+        }
     }
     
     [HttpGet("{userId}")]
     public async Task<IActionResult> GetNotificationsByUserId(int userId)
     {
-        var query = new GetNotificationsByUserIdQuery(userId);
-        var notifications = await notificationQueryService.Handle(query);
-        var notificationResources = notifications.Select(NotificationResourceFromEntityAssembler.ToResourceFromEntity);
-        return Ok(notificationResources);
+        try {
+            var query = new GetNotificationsByUserIdQuery(userId);
+            var notifications = await notificationQueryService.Handle(query);
+            var notificationResources = notifications.Select(NotificationResourceFromEntityAssembler.ToResourceFromEntity);
+            return Ok(notificationResources);
+        }catch(Exception ex) {
+            return BadRequest(new { message = ex.Message });
+        }
     }
     
     [HttpDelete("{notificationId}")]
     public async Task<IActionResult> DeleteNotification(int notificationId)
     {
-        var command = new DeleteNotificationCommand(notificationId);
-        var notification = await notificationCommandService.Handle(command);
-        return StatusCode(200, notification);
+        try {
+            var command = new DeleteNotificationCommand(notificationId);
+            var notification = await notificationCommandService.Handle(command);
+            return StatusCode(200, notification);
+        }catch(Exception ex) {
+            return BadRequest(new { message = ex.Message });
+        }
     }
 }
